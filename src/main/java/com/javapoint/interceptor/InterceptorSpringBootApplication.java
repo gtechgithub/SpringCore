c6a20b2b0ff343522f7e6ba9798bdccbcd7a8025
gtechgithub <36880236+gtechgithub@users.noreply.github.com>
2019-07-02 19:02:27 +0800

    Create InterceptorSpringBootApplication.java

---
+++ src/main/java/com/javapoint/interceptor/InterceptorSpringBootApplication.java
@@ -0,0 +1,12 @@
+package interceptorEX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterceptorSpringBootApplication {
+
+	public static void main(String args[]) {
+		SpringApplication.run(InterceptorSpringBootApplication.class, args);
+	}
+}
