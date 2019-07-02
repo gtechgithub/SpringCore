7689643df27134fe7d9d05d653920f93312de7a1
gtechgithub <36880236+gtechgithub@users.noreply.github.com>
2019-07-02 19:01:41 +0800

    Create StudentController.java

---
+++ src/main/java/com/javapoint/interceptor/StudentController.java
@@ -0,0 +1,24 @@
+package interceptorEX;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class StudentController {
+
+	@RequestMapping(value = "/getStudent")
+	public ResponseEntity<?> getStudent(){
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("SpringInterceptorConfig.xml");
+		StudentService studentService = (StudentService) context.getBean("studentService");
+		System.out.println("student:" + studentService.getStudent());
+		context.registerShutdownHook();
+		
+	      return new ResponseEntity<>(studentService.getStudent(), HttpStatus.OK);
+
+	}
+}
